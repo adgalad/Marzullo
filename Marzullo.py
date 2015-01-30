54027da0c65aa1d6a862b1c8b84b84592a3a3a64
JoseD92 <jose_daniel_d@hotmail.com>
2015-01-29 21:52:47 -0430

    Se agrego funcion para agregar reservaciones
    se agrego la posibilidad de guardar reservaciones en el estacionamiento, refactorizado los casos de prueba

--- Marzullo.py
+++ Marzullo.py
@@ -2,8 +2,15 @@ class parking:
     def __init__(self, capacity):
         self.occupation = []
         self.capacity = capacity
-
-    def fits(self, startTime, endTime):
-        if self.capacity > 0:
+    def addReservation(self,startTime,endTime):
+        startTimeVal=int(startTime)
+        endTimeVal=int(endTime)
+        if(startTimeVal>endTimeVal):
+            raise ValueError("Hora de inicio no puede ser mayor a hora final.")
+        tupla1 = (startTimeVal,1)
+        tupla2 = (endTimeVal,-1)
+        self.occupation+=[tupla1,tupla2]
+    def fits(self):
+        if self.capacity >= len(self.occupation)/2:
             return True
         return False
--- test.py
+++ test.py
@@ -13,15 +13,64 @@ class Test(unittest.TestCase):
 
     def testNoReservations(self):
         myParking = parking(1)
-        self.assertTrue(myParking.fits(10, 15))
+        self.assertTrue(myParking.fits())
 
     def testNoSpotParking(self):
         myParking = parking(0)
-        self.assertFalse(myParking.fits(10, 15))
+        myParking.addReservation("0900","1800")
+        self.assertFalse(myParking.fits())
         
     def testTenSpotsOneReservation(self):
         myParking = parking(10)
-        self.assertTrue(myParking.fits(10, 15))
+        myParking.addReservation("0900","1800")
+        self.assertTrue(myParking.fits())
+
+    def testNoAceptaAdd(self):
+        myParking = parking(10)
+        msg = "no debe agregar"
+        self.assertRaises(ValueError,myParking.addReservation,"0900","0600")
+
+    def testAddTwoReservation(self):
+        myParking = parking(10)
+        myParking.addReservation("0900","1800")
+        myParking.addReservation("0900","1800")
+        self.assertEqual(myParking.occupation,[(900,1),(1800,-1),(900,1),(1800,-1)])
+
+    def testAddTreeReservation(self):
+        myParking = parking(10)
+        myParking.addReservation("0900","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1200","1800")
+        self.assertEqual(myParking.occupation,[(900,1),(1800,-1),(1000,1),(1800,-1),(1200,1),(1800,-1)])
+
+    def testFiveSpotsFiveReservation(self):
+        myParking = parking(5)
+        myParking.addReservation("0900","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1200","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1000","1800")
+        self.assertTrue(myParking.fits())
+
+    def testFiveSpotsSixReservation(self):
+        myParking = parking(5)
+        myParking.addReservation("0900","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1200","1800")
+        myParking.addReservation("1200","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1000","1800")
+        self.assertFalse(myParking.fits())
+
+    def testFiveSpotsSixReservationNoOverlapping(self):
+        myParking = parking(5)
+        myParking.addReservation("0900","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("1200","1800")
+        myParking.addReservation("1200","1800")
+        myParking.addReservation("1000","1800")
+        myParking.addReservation("0600","1000")
+        self.assertTrue(myParking.fits())
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
