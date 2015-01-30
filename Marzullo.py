155bd536cb96905f6c51ac616d58d3420faa236e
akrmn <unacson@gmail.com>
2015-01-29 22:02:32 -0430

    translated error message

--- Marzullo.py
+++ Marzullo.py
@@ -2,14 +2,16 @@ class parking:
     def __init__(self, capacity):
         self.occupation = []
         self.capacity = capacity
+
     def addReservation(self,startTime,endTime):
         startTimeVal=int(startTime)
         endTimeVal=int(endTime)
         if(startTimeVal>endTimeVal):
-            raise ValueError("Hora de inicio no puede ser mayor a hora final.")
+            raise ValueError("startTime must be earlier than endTime.")
         tupla1 = (startTimeVal,1)
         tupla2 = (endTimeVal,-1)
         self.occupation+=[tupla1,tupla2]
+
     def fits(self):
         if self.capacity >= len(self.occupation)/2:
             return True
